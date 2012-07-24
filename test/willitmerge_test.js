24c2e454a17440349e6ff025e1197d22640f0b2a
Kyle Robinson Young <kyle@dontkry.com>
2012-07-23 21:31:45 -0700

    add more tests

--- test/willitmerge_test.js
+++ test/willitmerge_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var wim = require('../lib/willitmerge');
+var grunt = require('grunt');
 
 var oldStdout;
 
@@ -14,15 +15,34 @@ exports['willitmerge'] = {
     process.stdout.write = oldStdout;
     done();
   },
-  'get current user and repo': function(test) {
-    test.expect(2);
-    var oldFunc = wim.getIssues;
-    wim.getIssues = function() {
-      test.equal(this.options.user, 'shama');
-      test.equal(this.options.repo, 'willitmerge');
-      wim.getIssues = oldFunc;
+  'find remote, user and repo': function(test) {
+    test.expect(3);
+    wim.findRemote(function(err, remote) {
+      test.equal(remote, 'origin');
+      test.equal(wim.options.user, 'shama');
+      test.equal(wim.options.repo, 'willitmerge');
       test.done();
-    };
-    wim.run();
+    });
+  },
+  'get issues from github': function(test) {
+    test.expect(1);
+    wim.options.repo = 'Hello-World';
+    wim.options.user = 'octocat';
+    wim.options.perpage = 1;
+    wim.once('issues', function(issues) {
+      test.ok(issues.length === 1);
+      test.done();
+    });
+    wim.getIssues();
+  },
+  'test issues': function(test) {
+    test.expect(2);
+    var fixture = grunt.file.readJSON('test/fixtures/issues.json');
+    wim.once('end', function(issues) {
+      test.equal(issues[0].willitmerge.status, 'success');
+      test.equal(issues[0].willitmerge.impact, 0);
+      test.done();
+    });
+    wim.testIssues(fixture);
   }
 };
